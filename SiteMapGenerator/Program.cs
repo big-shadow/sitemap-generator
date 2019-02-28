5c5cb1b27886940911f97f3bba9c3ccb27a46a2d
Ray Winkelman <raywinkelman@gmail.com>
2019-02-28 04:27:44 +0000

    done

--- SiteMapGenerator/Program.cs
+++ SiteMapGenerator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace SiteMapGenerator
 {
@@ -6,7 +8,35 @@ namespace SiteMapGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var date = DateTime.Today.ToString("yyyy-MM-dd");
+
+            using (var fileStream = File.OpenRead("URLs.txt"))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, 128))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine("<url>");
+                    Console.WriteLine($" <loc>{line}</loc>");
+                    Console.WriteLine($" <lastmod>{date}</lastmod>");
+
+                    if (line.EndsWith(".com/"))
+                        Console.WriteLine(" <changefreq>weekly</changefreq>");                  
+                    else
+                        Console.WriteLine(" <changefreq>monthly</changefreq>");
+
+                    if (line.Contains("gift-guides"))
+                        Console.WriteLine(" <priority>0.8000</priority>");
+                    else if (line.EndsWith(".com/"))
+                        Console.WriteLine(" <priority>1.0000</priority>");
+                    else
+                        Console.WriteLine(" <priority>0.5000</priority>");
+
+                    Console.WriteLine("</url>");
+                }
+            }
+
+            Console.Read();
         }
     }
 }
